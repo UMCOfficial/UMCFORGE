85ade9cd74a723245c269d95fa80fb53111feba2
marshmello1YT <unconfigured@null.spigotmc.org>
2022-07-29 19:15:52 +0300

    backup Changes: Advancements

--- src/main/java/forge/procedures/WBGUIPProcedure.java
+++ src/main/java/forge/procedures/WBGUIPProcedure.java
@@ -103,5 +103,147 @@ public class WBGUIPProcedure {
 				}
 			}
 		}
+		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get(0)).getItem()
+				: ItemStack.EMPTY).getItem() == UmcforgeModItems.DISCERITE.get()) {
+			if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+					&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+					.getItem() == UmcforgeModBlocks.MAHOGANY_PLANKS.get().asItem()) {
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY)
+						.getItem() == UmcforgeModItems.DISCERITE.get()) {
+					if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY)
+							.getItem() == Blocks.DIAMOND_BLOCK.asItem()) {
+						if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+								&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(4)).getItem() : ItemStack.EMPTY)
+								.getItem() == Blocks.GLOWSTONE.asItem()) {
+							if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+									&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(5)).getItem() : ItemStack.EMPTY)
+									.getItem() == Blocks.DIAMOND_BLOCK.asItem()) {
+								if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+										&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(6)).getItem() : ItemStack.EMPTY)
+										.getItem() == UmcforgeModItems.DISCERITE.get()) {
+									if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+											&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(7)).getItem() : ItemStack.EMPTY)
+											.getItem() == UmcforgeModBlocks.MAHOGANY_PLANKS.get().asItem()) {
+										if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+												&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(8)).getItem() : ItemStack.EMPTY)
+												.getItem() == UmcforgeModItems.DISCERITE.get()) {
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(0)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(1)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(2)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(3)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(4)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(5)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(6)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(7)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(8)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												ItemStack _setstack = new ItemStack(UmcforgeModBlocks.DISC_BENCH.get());
+												_setstack.setCount(1);
+												((Slot) _slots.get(9)).set(_setstack);
+												_player.containerMenu.broadcastChanges();
+											}
+										} else {
+											if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+													&& _current.get() instanceof Map _slots) {
+												((Slot) _slots.get(9)).remove(1);
+												_player.containerMenu.broadcastChanges();
+											}
+										}
+									} else {
+										if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+												&& _current.get() instanceof Map _slots) {
+											((Slot) _slots.get(9)).remove(1);
+											_player.containerMenu.broadcastChanges();
+										}
+									}
+								} else {
+									if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+											&& _current.get() instanceof Map _slots) {
+										((Slot) _slots.get(9)).remove(1);
+										_player.containerMenu.broadcastChanges();
+									}
+								}
+							} else {
+								if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+										&& _current.get() instanceof Map _slots) {
+									((Slot) _slots.get(9)).remove(1);
+									_player.containerMenu.broadcastChanges();
+								}
+							}
+						} else {
+							if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+									&& _current.get() instanceof Map _slots) {
+								((Slot) _slots.get(9)).remove(1);
+								_player.containerMenu.broadcastChanges();
+							}
+						}
+					} else {
+						if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+								&& _current.get() instanceof Map _slots) {
+							((Slot) _slots.get(9)).remove(1);
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+				} else {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(9)).remove(1);
+						_player.containerMenu.broadcastChanges();
+					}
+				}
+			} else {
+				if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+						&& _current.get() instanceof Map _slots) {
+					((Slot) _slots.get(9)).remove(1);
+					_player.containerMenu.broadcastChanges();
+				}
+			}
+		} else {
+			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+					&& _current.get() instanceof Map _slots) {
+				((Slot) _slots.get(9)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
 	}
 }
